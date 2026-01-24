8ae600b0eb3cdfe03c2ce8cedc4bde3adea011db
saori <matsu9789@gmail.com>
2026-01-24 18:45:17 +0900

    ヘッダー作成

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -6,13 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>coachtechフリマ</title>
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
     @yield('css')
 </head>
 
 <body>
-    <header>
-        <img src="" alt="">
-        COACHTECH
+    <header class="header">
+        <div class="header_space">
+            <img class="header_img" src="{{ asset('images/COACHTECHヘッダーロゴ.png') }}" alt="COACHTECH">
+        </div>
     </header>
     <main>
         @yield('content')

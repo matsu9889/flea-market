ac46f8a562fcd605d9a06832c972ad0d82991cf5
saori <matsu9789@gmail.com>
2026-01-25 13:49:52 +0900

    会員登録CSS

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -5,27 +5,32 @@
 @endsection
 
 @section('content')
-<h2>会員登録</h2>
-<form action="/register" method="post">
-    <div>
-        <label for="name">ユーザー名</label>
-        <input type="text" name="name" id="name" value="{{ old('name') }}">
+<div class="form-container">
+    <h2 class="title">会員登録</h2>
+    <form action="/register" method="post">
+        @csrf
+        <div class="form-inner">
+            <div class="form-group">
+                <label class="form-group_label" for="name">ユーザー名</label>
+                <input class="form-group_input" type="text" name="name" id="name" value="{{ old('name') }}">
+            </div>
+            <div class="form-group">
+                <label class="form-group_label" for="email">メールアドレス</label>
+                <input class="form-group_input" type="email" name="email" id="email" value="{{ old('email') }}">
+            </div>
+            <div class="form-group">
+                <label class="form-group_label" for="password">パスワード</label>
+                <input class="form-group_input" type="password" name="password" id="password">
+            </div>
+            <div class="form-group">
+                <label class="form-group_label" for="password_confirmation">確認用パスワード</label>
+                <input class="form-group_input" type="password" name="password_confirmation" id="password_confirmation">
+            </div>
+        </div>
+        <button type="submit" class="button">登録する</button>
+    </form>
+    <div class="link">
+        <a class="link__inner" href="/login">ログインはこちら</a>
     </div>
-    <div>
-        <label for="email">メールアドレス</label>
-        <input type="email" name="email" id="email" value="{{ old('email') }}">
-    </div>
-    <div>
-        <label for="password">パスワード</label>
-        <input type="password" name="password" id="password">
-    </div>
-    <div>
-        <label for="password_confirmation">確認用パスワード</label>
-        <input type="password" name="password_confirmation" id="password_confirmation">
-    </div>
-    <button>登録</button>
-</form>
-<div>
-    <a href="">ログインはこちら</a>
 </div>
 @endsection

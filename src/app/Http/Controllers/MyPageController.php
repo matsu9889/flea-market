a0f6be52bda3d610102da4d784f1a2f2966f778d
saori <matsu9789@gmail.com>
2026-02-01 18:14:27 +0900

    既存の値取得

--- src/app/Http/Controllers/MyPageController.php
+++ src/app/Http/Controllers/MyPageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MyPageController extends Controller
 {
@@ -14,11 +15,18 @@ class MyPageController extends Controller
 
     public function edit()
     {
-        return view('mypage.edit');
+        $user = Auth::user();
+        return view('mypage.edit',compact('user'));
     }
 
     public function update(Request $request)
     {
+        $user = Auth::user();
+        // 画像がアップロードされたときだけ
+        if ($request->hasFile('image')) {
+            $path = $request->file('image')->store('profile', 'public');
+            $user->image = $path;
+        }
         $profile = $request->only(['user_name', 'post_code', 'address', 'building']);
         return view('mypage.show', compact('profile'));
     }
--- src/resources/views/mypage/edit.blade.php
+++ src/resources/views/mypage/edit.blade.php
@@ -7,31 +7,32 @@
 @section('content')
 <div class="form-container">
     <h2 class="title">プロフィール設定</h2>
-    <form action="/mypage/profile" method="POST">
+    <form action="/mypage/profile" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="image-group">
             <div class="image">
                 <img src="" alt="プロフィール画像">
             </div>
             <div>
-                <button class="select-button">画像を選択する</button>
+                <label class="image-button" for="image">画像を選択する</label>
+                <input class="image-input" type="file" name="image" id="image">
             </div>
         </div>
         <div class="form-inner">
             <div class="form-group">
-                <label class="form-group_label" for="user">ユーザー名</label>
-                <input class="form-group_input" type="text" name="user" id="user" value="{{ old('user') }}">
-                <div class="user">
-                    @error('user')
+                <label class="form-group_label" for="user_name">ユーザー名</label>
+                <input class="form-group_input" type="text" name="user_name" id="user_name" value="{{ old('user_name', $user->user_name) }}">
+                <div class="error">
+                    @error('user_name')
                     {{ $message }}
                     @enderror
                 </div>
             </div>
             <div class="form-group">
-                <label class="form-group_label" for="post">郵便番号</label>
-                <input class="form-group_input" type="number" name="post" id="post" value="{{ old('post') }}">
+                <label class="form-group_label" for="post_code">郵便番号</label>
+                <input class="form-group_input" type="number" name="post_code" id="post_code" value="{{ old('post_code') }}">
                 <div class="error">
-                    @error('post')
+                    @error('post_code')
                     {{ $message }}
                     @enderror
                 </div>
@@ -47,7 +48,7 @@
             </div>
             <div class="form-group">
                 <label class="form-group_label" for="building">建物名</label>
-                <input class="form-group_input" type="building" name="building" id="building">
+                <input class="form-group_input" type="building" name="building" id="building" value="{{ old('building') }}">
                 <div class="error">
                     @error('building')
                     {{ $message }}

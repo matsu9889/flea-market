1b10da9631a5663c651abfbc33480da1079e21e1
saori <matsu9789@gmail.com>
2026-02-05 18:24:45 +0900

    マイグレーションファイル・モデル作成

---
+++ src/database/migrations/2026_02_03_182252_create_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('item_name');
+            $table->integer('price');
+            $table->string('brand_name');
+            $table->string('description');
+            $table->string('img_url');
+            $table->string('category');
+            $table->string('condition');
+            $table->boolean('purchased_flag')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}

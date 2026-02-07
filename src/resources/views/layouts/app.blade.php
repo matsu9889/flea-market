cf356cc62c957afea3937d5f6febcad7fb929a43
saori <matsu9789@gmail.com>
2026-02-07 11:43:38 +0900

    ヘッダー編集

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -12,19 +12,31 @@
 
 <body>
     <header class="header">
-        <div class="header_space">
+        <div class="header_logo">
             <img class="header_img" src="{{ asset('images/COACHTECHヘッダーロゴ.png') }}" alt="COACHTECH">
         </div>
         @if (Auth::check())
-        <li class="header-nav__item">
-            <a class="header-nav__link" href="/mypage">マイページ</a>
-        </li>
-        <li class="header-nav__item">
-            <form action="/logout" method="post">
-                @csrf
-                <button class="header-nav__button">ログアウト</button>
-            </form>
-        </li>
+
+        <ul class="header-nav">
+            <li class="header-nav__item">
+                <form action="find" method="POST">
+                    @csrf
+                    <input class="header-nav__" input type="text" name="input" value="" placeholder="なにをお探しですか？">
+                </form>
+            </li>
+            <li class="header-nav__item">
+                <form action="/logout" method="post">
+                    @csrf
+                    <button class="header-nav__logout">ログアウト</button>
+                </form>
+            </li>
+            <li class="header-nav__item">
+                <a class="header-nav__mypage" href="/mypage">マイページ</a>
+            </li>
+            <li class="header-nav__item">
+                <a class="header-nav__listing" href="">出品</a>
+            </li>
+        </ul>
         @endif
     </header>
     <main>

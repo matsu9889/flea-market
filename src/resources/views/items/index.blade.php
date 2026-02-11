28b6cfa9d32fae3dc9087d5fb7f882d781eb4a34
saori <matsu9789@gmail.com>
2026-02-11 16:00:28 +0900

    商品一覧画面作成

--- src/resources/views/items/index.blade.php
+++ src/resources/views/items/index.blade.php
@@ -2,4 +2,49 @@
 
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="buttons">
+        <button class="recommend-button">おすすめ</button>
+        <button class="mylist-button">マイリスト</button>
+    </div>
+
+    <div class="screen">
+        <div class="listing-container">
+            <div class="listing-item">
+                <div class="listing-item-img">
+                    <img src="" alt="商品画像">
+                </div>
+                <p>商品名</p>
+            </div>
+            <div class="listing-item">
+                <div class="listing-item-img">
+                    <img src="" alt="商品画像">
+                </div>
+                <p>商品名</p>
+            </div>
+            <div class="listing-item">
+                <div class="listing-item-img">
+                    <img src="" alt="商品画像">
+                </div>
+                <p>商品名</p>
+            </div>
+            <div class="listing-item">
+                <div class="listing-item-img">
+                    <img src="" alt="商品画像">
+                </div>
+                <p>商品名</p>
+            </div>
+            <div class="listing-item">
+                <div class="listing-item-img">
+                    <img src="" alt="商品画像">
+                </div>
+                <p>商品名</p>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection

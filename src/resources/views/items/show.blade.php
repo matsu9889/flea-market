3a7c240f6d50b228bae033464fcb13758e49de78
saori <matsu9789@gmail.com>
2026-02-28 16:58:34 +0900

    商品詳細画面CSS修正

--- src/resources/views/items/show.blade.php
+++ src/resources/views/items/show.blade.php
@@ -13,24 +13,28 @@
 
     <div class="item-detail">
         <!-- 商品名 -->
-        <p class="item__name">{{ $item->item_name }}</p>
+        <h1 class="item__name">{{ $item->item_name }}</h1>
         <!-- ブランド名 -->
         <p class="item__brand">{{ $item->brand_name }}</p>
         <!-- 金額 -->
         <p class="item__price--yen">&yen;<span class="item__price">{{ $item->price }}</span>(税込)</p>
         <!-- いいねボタン、コメントボタン -->
-        <div class="item__icon">
-            <form action="/item/{{ $item->id }}/favorite" method="POST">
-                @csrf
-                @if($item->is_liked_by_auth_user())
-                <input class="item__icon-heart--action" type="image" src="{{ asset('images/ハートロゴ_ピンク.png') }}" alt="ハートピンク">
-                @else
-                <input class="item__icon-heart" type="image" src="{{ asset('images/ハートロゴ_デフォルト.png') }}" alt="ハート">
-                @endif
-            </form>
-            <input class="item__icon-speech" type="image" src="{{ asset('images/ふきだしロゴ.png') }}" alt="ふきだし">
-            <div>{{ $item->favorites_count }}</div>
-            <div>{{ $item->comments_count }}</div>
+        <div class="item__icon--container">
+            <div class="item__icon">
+                <form action="/item/{{ $item->id }}/favorite" method="POST">
+                    @csrf
+                    @if($item->is_liked_by_auth_user())
+                    <input type="image" src="{{ asset('images/ハートロゴ_ピンク.png') }}" alt="ハートピンク">
+                    @else
+                    <input type="image" src="{{ asset('images/ハートロゴ_デフォルト.png') }}" alt="ハート">
+                    @endif
+                </form>
+                <div class="item__icon--count">{{ $item->favorites_count }}</div>
+            </div>
+            <div class="item__icon">
+                <input type="image" src="{{ asset('images/ふきだしロゴ.png') }}" alt="ふきだし">
+                <div class="item__icon--count">{{ $item->comments_count }}</div>
+            </div>
         </div>
         <div class="item__link">
             <a class="item__link--purchase" href="/purchase/{{ $item->id }}">購入手続きへ</a>
@@ -47,21 +51,21 @@
             <p class="item__section-condition">{{ $item->condition }}</p>
         </div>
         <div class="item__section">
-            <h2 class="item__section-comment">コメント</h2>
+            <h2 class="item__section-comment">コメント({{ $item->comments_count }})</h2>
+            @foreach($item->comments as $comment)
             <div class="item__section-comment__list">
-                @foreach($item->comments as $comment)
-                {{ $comment->user->image }}
-                {{ $comment->user->user_name }}
-                {{ $comment->content }}
-                @endforeach
+                <img class="item__comment--user-image" src="{{ asset('storage/' . $comment->user->image) }}" alt="ユーザー画像">
+                <div class="item__comment--user-name">{{ $comment->user->user_name }}</div>
+                <div class="item__comment--content">{{ $comment->content }}</div>
             </div>
+            @endforeach
         </div>
         <div class="item__section">
             <form action="/item/{{ $item->id }}/comment" method="POST">
                 @csrf
                 <h2 class="item__section-comment__title">商品へのコメント</h2>
                 <textarea class="item__section-comment__textarea" name="content">{{ old('content') }}</textarea>
-                <button class="item__button">コメントを送信する</button>
+                <button class="item__section-comment__button">コメントを送信する</button>
             </form>
         </div>
     </div>

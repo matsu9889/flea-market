0d9e4efbe24d9cfdd9c5abb27ee9f4bb2120c380
saori <matsu9789@gmail.com>
2026-03-07 16:39:04 +0900

    マイページ情報取得

--- src/resources/views/mypage/show.blade.php
+++ src/resources/views/mypage/show.blade.php
@@ -7,8 +7,8 @@
 @section('content')
 <form action="/mypage" method="GET">
     <div class="profile">
-        <div class="profile__image">
-            <img src="{{ asset('storage/' . $user->image) }}" alt="プロフィール画像">
+        <div class="profile__image-area">
+            <img class="profile__image" src="{{ asset('storage/' . $user->image) }}" alt="プロフィール画像">
         </div>
         <div class="profile__user-name">
             <h2>{{ $user->user_name }}</h2>
@@ -19,52 +19,50 @@
     </div>
 
 
-    <div class="buttons">
-        <button class="listing-button">出品した商品</button>
-        <button class="purchase-button">購入した商品</button>
+    <div class="tab-buttons">
+        <button type="button" class="tab-button active" data-tab="listing">出品商品</button>
+        <button type="button" class="tab-button" data-tab="purchase">購入商品</button>
     </div>
-    <div class="screen">
+    <div class="tab-contents">
         <!-- 出品画面 -->
-        <div class="listing-container">
-            <div class="listing-item">
-                <div class="listing-item-img">
-                    <img src="" alt="商品画像">
+        <div class="tab-content active" id="listing">
+            <div class="listing-container">
+                @foreach($listingItems as $listingItem)
+                <div class="listing-item">
+                    <div class="listing-item__image-area">
+                        <img class="listing-item__image" src="{{ asset('storage/' . $listingItem->img_url) }}" alt="商品画像">
+                    </div>
+                    <p class="listing-item-name">{{ $listingItem->item_name }}</p>
                 </div>
-                <p>商品名</p>
-            </div>
-            <div class="listing-item">
-                <div class="listing-item-img">
-                    <img src="" alt="商品画像">
-                </div>
-                <p>商品名</p>
-            </div>
-            <div class="listing-item">
-                <div class="listing-item-img">
-                    <img src="" alt="商品画像">
-                </div>
-                <p>商品名</p>
-            </div>
-            <div class="listing-item">
-                <div class="listing-item-img">
-                    <img src="" alt="商品画像">
-                </div>
-                <p>商品名</p>
-            </div>
-            <div class="listing-item">
-                <div class="listing-item-img">
-                    <img src="" alt="商品画像">
-                </div>
-                <p>商品名</p>
+                @endforeach
             </div>
         </div>
         <!-- 購入画面 -->
-        <div>
-            <img src="purchase" alt="">
-            <p>商品名</p>
+        <div class="tab-content" id="purchase">
+            <div class="purchase-container">
+                @foreach($purchaseItems as $purchaseItem)
+                <div class="purchase-item">
+                    <div class="purchase-item__image-area">
+                        <img class="purchase-item__image" src="{{ asset('storage/' . $purchaseItem->img_url) }}" alt="商品画像">
+                    </div>
+                    <p class="purchase-item-name">{{ $purchaseItem->item_name }}</p>
+                </div>
+                @endforeach
+            </div>
         </div>
     </div>
-
 </form>
 
+<script>
+    document.querySelectorAll('.tab-button').forEach(button => {
+        button.addEventListener('click', function() {
+            document.querySelectorAll('.tab-button').forEach(btn => btn.classList.remove('active'));
+            document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active'));
 
+            this.classList.add('active');
+            const tabName = this.dataset.tab;
+            document.getElementById(tabName).classList.add('active');
+        });
+    });
+</script>
 @endsection

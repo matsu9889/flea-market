f0d37024ba4f31124282c3d5b0a308c155662c99
saori <matsu9789@gmail.com>
2026-03-08 17:05:21 +0900

    会員登録テスト

--- src/tests/Feature/RegisterTest.php
+++ src/tests/Feature/RegisterTest.php
@@ -30,4 +30,79 @@ class RegisterTest extends TestCase
 
         $response->assertSessionHasErrors(['user_name' => 'お名前を入力してください']);
     }
+
+    public function test_create_post_requires_email()
+    {
+        $data = [
+            'user_name' => 'Taro',
+            'email' => '',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ];
+
+        $response = $this->post('/register', $data);
+
+        $response->assertSessionHasErrors(['email' => 'メールアドレスを入力してください']);
+    }
+
+    public function test_create_post_requires_password()
+    {
+        $data = [
+            'user_name' => 'Taro',
+            'email' => 'test@example.com',
+            'password' => '',
+            'password_confirmation' => '',
+        ];
+
+        $response = $this->post('/register', $data);
+
+        $response->assertSessionHasErrors(['password' => 'パスワードを入力してください']);
+    }
+
+    public function test_create_post_minimum_8_password()
+    {
+        $data = [
+            'user_name' => 'Taro',
+            'email' => 'test@example.com',
+            'password' => '1234567',
+            'password_confirmation' => '1234567',
+        ];
+
+        $response = $this->post('/register', $data);
+
+        $response->assertSessionHasErrors(['password' => 'パスワードは8文字以上で入力してください']);
+    }
+
+    public function test_create_post_match_password()
+    {
+        $data = [
+            'user_name' => 'Taro',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => '12345678',
+        ];
+
+        $response = $this->post('/register', $data);
+
+        $response->assertSessionHasErrors(['password' => 'パスワードと一致しません']);
+    }
+
+    public function test_successful_registration_redirects_to_profile()
+    {
+        $data = [
+            'user_name' => 'Taro',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ];
+
+        $response = $this->post('/register', $data);
+
+        $response->assertRedirect('/mypage/profile');
+
+        $this->assertDatabaseHas('users', [
+            'email' => 'test@example.com',
+            'user_name' => 'Taro',
+        ]);
+    }
 }
